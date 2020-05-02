d10b37572494e568737337df9cd80178a5cb116f
Gilian Brouwer <nghtbaron@gmail.com>
2020-05-03 01:37:03 +0200

    test

--- app.py
+++ app.py
@@ -6,11 +6,9 @@ Created on Tue Mar 10 15:15:16 2020
 """
 from flask import Flask, render_template
 import pandas as pd
-from sklearn.neighbors import NearestNeighbors
 
 df2 = pd.read_parquet('data/matrix.parquet')
 anime=pd.read_csv('anime2.csv',  usecols= ['anime_id', 'title', 'title_english', 'genre'])
-model_knn=NearestNeighbors(metric='cosine', algorithm='brute')
 
 app = Flask(__name__)
 
@@ -22,37 +20,10 @@ def my_form():
     
 def recommender():
     
-    def make_recommendation(model_knn, data, mapper, n_recommendations):
-        # fit
-        model_knn.fit(data)
-        # get input anime
-        idx = 1090
-        distances, indices = model_knn.kneighbors(data.iloc[idx, :].values.reshape(1, -1), n_neighbors=n_recommendations+1)
-        # print recommendations
-        ind= indices.tolist()
-        dist=sorted(list(zip(distances.tolist(), indices.tolist())))
-        ds=pd.DataFrame(dist[0]).transpose()
-        recom = mapper.iloc[ind[0], :]
-        
-        ds[1]=ds[1].astype('int64')
-        recom=recom.reset_index(drop=True)
-        
-        recommer= pd.concat([recom, ds], axis=1)
-        recani=recommer.loc[0, 'title']
-        recommer= recommer.drop([0])
-        recommer=recommer.sort_values(by=[0], ascending=False)
-        recommer=recommer.drop([0, 1], axis=1)
-        recommer=recommer.rename(columns={'anime_id': 'ID', 'title' : 'Title', 'title_english' : 'English Title', 'genre' : 'Genre'})
-        recommer=recommer.set_index('Title')
-        
-        return recani, recommer
     
+    recommer=anime.head()
         
-    recani, recommer=make_recommendation(
-        model_knn=model_knn,
-        data=df2,
-        mapper=anime,
-        n_recommendations=5)
+    recani='Zero no Tsukaima'
     
     return render_template('index.html',  recani=recani, tables=[recommer.to_html(classes='data')], titles=recommer.columns.values)
 
